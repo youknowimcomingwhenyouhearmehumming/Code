251e399b83e4825d97eeadcf774cd4851d2eea9a
RasmusRiis <ralle_riis@hotmail.com>
2019-05-01 20:00:43 +0200

    fsdhd

--- KNN_SVM_RNN_new_data/RVM_optimizationAndTest.py
+++ KNN_SVM_RNN_new_data/RVM_optimizationAndTest.py
@@ -10,6 +10,8 @@ import numpy as np
 import os
 import pickle
 from skrvm import RVC
+from sklearn.model_selection import GridSearchCV
+
 #from sklearn.model_selection import GridSearchCV
 
 #For the test of comparing KNN/SVM/RVM basic
@@ -30,13 +32,50 @@ full_normPCA655_array = np.load('full_normPCA655_array.npy')
 full_normPCA128_array = np.load('full_normPCA128_array.npy')
 #######################################################################
 
+
+def rvc_param_selection(X, y, nfolds): #https://medium.com/@aneesha/svm-parameter-tuning-in-scikit-learn-using-gridsearchcv-2413c02125a0
+    gammas = [1e-8,1e-7,1e-6, 1e-5,1e-4,1e-3,1e-2]
+    param_grid = {'coef1' : gammas}
+    grid_search = GridSearchCV(RVC(kernel='rbf'), param_grid, cv=nfolds)
+    grid_search.fit(X, y)
+    grid_search.best_params_
+    return grid_search.best_params_
+
+def rvc_param_selection2(X, y, nfolds): #https://medium.com/@aneesha/svm-parameter-tuning-in-scikit-learn-using-gridsearchcv-2413c02125a0
+    gammas = [1e-4,1e-3,1e-2]
+    param_grid = {'coef1' : gammas}
+    grid_search = GridSearchCV(RVC(kernel='rbf'), param_grid, cv=nfolds)
+    grid_search.fit(X, y)
+    grid_search.best_params_
+    return grid_search.best_params_
+
+
+
+
 #WE START with the biary case
+    
+X = full_normPCA128_array[train_indicies]
+Y = full_isAnimal_array[train_indicies]
+
+params = rvc_param_selection(X,Y,5)
+
+params = rvc_param_selection2(X,Y,5)
+
+
 
 ####################TO TEST
-clf = RVC(kernel='rbf',alpha=1e-08, beta=1e-08,)
+clf = RVC(kernel='rbf',coef1=0.1)# coef1:  1=46
 clf.fit(full_normPCA128_array[train_indicies], full_isAnimal_array[train_indicies])
+#clf.score(full_normPCA128_array[train_indicies],full_isAnimal_array[train_indicies])
 clf.score(full_normPCA128_array[test_indicies],full_isAnimal_array[test_indicies])
 
-clf = RVC(kernel='rbf',alpha=1e-08, beta=1e-08,)
-clf.fit(full_normPCA128_array[train_indicies], full_isAnimal_array[train_indicies])
-clf.score(full_normPCA128_array[test_indicies],full_isAnimal_array[test_indicies])
+predictions = clf.predict(full_normPCA128_array[test_indicies])
+
+corr=0
+for i in range(np.size(predictions)):
+    if predictions[i] == full_isAnimal_array[test_indicies][i]:
+        corr += 1
+        
+        
+        
+        
